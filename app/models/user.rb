fc0fb81db5bd4faa36d8f0d679843279ce83cb1b
Taiki <taiki.1230.3163@icloud.com>
2020-07-30 10:57:45 +0000

    task4-1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX, }
   VALID_PASSWORD_REGEX = /\A(?=\d{0,99}+[a-z])(?=[a-z]{0,99}\d)[a-z\d]{8,32}+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX, } 
+  
   has_secure_password
+  
+  has_many :topics
 end
